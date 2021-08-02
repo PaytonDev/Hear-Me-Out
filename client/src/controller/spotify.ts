3337a7b4b30aec79254f4272c4ede981a84a844b
Breeana Payton <breeana.payton@gmail.com>
2021-08-02 14:20:32 -0500

    Deployment configuration update

--- client/src/controller/spotify.ts
+++ client/src/controller/spotify.ts
@@ -1,6 +1,8 @@
 const authURL = 'https://accounts.spotify.com/authorize';
 const redirectURi = 'https://hear-me-out-spotify-api.herokuapp.com/'
-const clientId = process.env.Client_ID
+const clientId = '76007946b07a474487db86cb749ba027'
+
+
 
 const scopes = [
     'streaming',
