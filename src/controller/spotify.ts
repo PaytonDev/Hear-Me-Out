a2575091ed237755fadbaec8ab6bc353bb3f5114
Breeana Payton <breeana.payton@gmail.com>
2021-12-14 09:09:20 -0600

    updated login url

--- src/controller/spotify.ts
+++ src/controller/spotify.ts
@@ -1,1 +1,5 @@
-export const loginURL = "http://localhost:4000/login";
+export const loginURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(
+  "76007946b07a474487db86cb749ba027"
+)}&redirect_uri=${encodeURIComponent("http://localhost:3000/")}&scope=${encodeURIComponent(
+  "streaming user-read-email user-read-private"
+)}`;
