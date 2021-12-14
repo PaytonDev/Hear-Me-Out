dd0884fe377e6f6396cac9021b3aeece90ddb870
Breeana Payton <breeana.payton@gmail.com>
2021-12-13 18:46:33 -0600

    Updated Login url

--- src/controller/spotify.ts
+++ src/controller/spotify.ts
@@ -1,4 +1,4 @@
-export const loginURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(
+export const login = `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(
   "76007946b07a474487db86cb749ba027"
 )}&redirect_uri=${encodeURIComponent(
   "https://hear-me-out-43d23.web.app/"
